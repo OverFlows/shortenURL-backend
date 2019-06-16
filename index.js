32600a21fb280605a724d4eafb99bedc62e51b95
Florent Argod <florent@pc3.home>
2019-06-16 15:34:17 +0200

    add comment on index.js

--- index.js
+++ index.js
@@ -7,20 +7,22 @@ const cors = require("cors");
 app.use(cors());
 app.use(bodyParser.json());
 
+//Connect to DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/shortenurl",
   {
     useNewUrlParser: true
   },
-
   function(err) {
     if (err) console.error("Could not connect to mongodb.");
   }
 );
 
+//declare route(s)
 const redirectionRoutes = require("./routes/redirectionRoutes");
 app.use(redirectionRoutes);
 
+//Let's go!
 app.listen(process.env.PORT || 3001, () => {
   console.log("Server started");
 });
